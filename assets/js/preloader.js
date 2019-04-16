487051e5fd2ddc00a2c2691041c35c6c66d2848d
Michele <michelesmolensky@practicecafe.com>
2019-04-16 16:43:26 -0500

    adding in some stylish animations now

---
+++ assets/js/preloader.js
@@ -0,0 +1,26 @@
+//run loader
+
+
+// var interval = setInterval(() => {
+//     if (document.readyState === 'complete') {
+//         var preloader = document.getElementById('loading');
+//         var docLoad = document.querySelector('main');
+//         preloader.classList.remove('loading');
+//         preloader.classList.add('loaded');
+//         docLoad.classList.add('body-visible');
+
+//         clearInterval(interval);
+//     }
+// }, 4977);
+
+//load welcome section elements
+
+var headerLogo = document.querySelector('header .logo');
+var docLoad = document.querySelector('main');
+
+if(docLoad.classList == 'body-visible'){
+    anime({
+        targets: headerLogo,
+        opacity: 1
+    })
+}
--- public/js/all.js
+++ public/js/all.js
@@ -1,15 +1,3 @@
-var tabItems = document.querySelectorAll('.employment-list a'); 
-
-tabItems.forEach(function(tabItem){
-    tabItem.addEventListener('click', function(){
-        tabItems.forEach(function(tabItem){
-            tabItem.classList.remove('active');
-        })
-        tabItem.classList.add('active');
-    });
-})
-
-
 // // have events happen on scroll. 
 
 // //first have projects fade in on view.
@@ -36,6 +24,34 @@ tabItems.forEach(function(tabItem){
 // }
 // console.log(window.scrollY);
 // window.addEventListener('scroll', position);
-// const project = document.querySelector('.project');
 
-// project.classList.add('fadeIn');
+const project = document.querySelector('.project');
+
+project.classList.add('fadeIn');
+//run loader
+
+
+// var interval = setInterval(() => {
+//     if (document.readyState === 'complete') {
+//         var preloader = document.getElementById('loading');
+//         var docLoad = document.querySelector('main');
+//         preloader.classList.remove('loading');
+//         preloader.classList.add('loaded');
+//         docLoad.classList.add('body-visible');
+
+//         clearInterval(interval);
+//     }
+// }, 4977);
+
+//load welcome section elements
+
+var headerLogo = document.querySelector('header .logo');
+var docLoad = document.querySelector('main');
+
+if(docLoad.classList == 'body-visible'){
+    anime({
+        targets: headerLogo,
+        opacity: 1
+    })
+}
+// hide non-active employment tabs
