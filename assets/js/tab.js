e83000ecf8119398176b0243001ae5b4f62a2b1c
Michele Smolensky <michele.smolensky@gmail.com>
2019-04-15 20:32:27 -0500

    adding in some js

---
+++ assets/js/tab.js
@@ -0,0 +1,1 @@
+// hide non-active employment tabs
--- public/js/all.js
+++ public/js/all.js
@@ -1,29 +1,41 @@
-// have events happen on scroll. 
+var tabItems = document.querySelectorAll('.employment-list a'); 
 
-//first have projects fade in on view.
-
-
-let scrollPosition = 0;
-const projects = document.querySelectorAll('.project');
-
-
-function position(){
-    let windowY = window.scrollY;
-    if (windowY < scrollPosition){
-        console.log(scrollPosition);
-        projects.forEach((project) => {
-            project.classList.add('none');
+tabItems.forEach(function(tabItem){
+    tabItem.addEventListener('click', function(){
+        tabItems.forEach(function(tabItem){
+            tabItem.classList.remove('active');
         })
+        tabItem.classList.add('active');
+    });
+})
+
+
+// // have events happen on scroll. 
+
+// //first have projects fade in on view.
+
+
+// let scrollPosition = 0;
+// const projects = document.querySelectorAll('.project');
+
+
+// function position(){
+//     let windowY = window.scrollY;
+//     if (windowY < scrollPosition){
+//         console.log(scrollPosition);
+//         projects.forEach((project) => {
+//             project.classList.add('none');
+//         })
         
-    } else {
-        projects.forEach((project) => {
-            project.classList.add('else');
-        })
-    }
-    scrollPosition = windowY;
-}
-console.log(window.scrollY);
-window.addEventListener('scroll', position);
-const project = document.querySelector('.project');
+//     } else {
+//         projects.forEach((project) => {
+//             project.classList.add('else');
+//         })
+//     }
+//     scrollPosition = windowY;
+// }
+// console.log(window.scrollY);
+// window.addEventListener('scroll', position);
+// const project = document.querySelector('.project');
 
-project.classList.add('fadeIn');
+// project.classList.add('fadeIn');
