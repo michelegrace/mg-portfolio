fc9a2940d45779acc29ad08bc94f2020215bfa6c
Michele <michele@inleague.org>
2019-04-21 21:04:38 -0500

    finished up the animations after the preload

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,14 +1,38 @@
 // //run loader
-// var loadScreen = setInterval(() => {
-//     if (document.readyState === 'complete') {
-//         var preloader = document.getElementById('loading');
-//         var docLoad = document.querySelector('main');
-//         var headerLogo = document.querySelector('header .logo');
-//         docLoad.classList.remove('body-hidden');
-//         docLoad.classList.add('body-visible');
-//         preloader.classList.remove('loading');
-//         preloader.classList.add('loaded');
-//         headerLogo.classList.add('fadeInElem');
-//         clearInterval(loadScreen);
-//     }
-// }, 4977);
+
+function preload() {
+  setTimeout( () => {
+    if (document.readyState === 'complete') {
+    var preloader = document.getElementById('loading');
+    var docLoad = document.querySelector('main');
+    docLoad.classList.remove('body-hidden');
+    docLoad.classList.add('body-visible');
+    preloader.classList.remove('loading');
+    preloader.classList.add('loaded');
+    };
+  }, 4977);
+
+  setTimeout( ()=> {
+
+    var welcomeTimeLine = anime.timeline({
+        easing: 'linear',
+        duration: 1000
+    });
+
+    welcomeTimeLine.add({
+        targets: ['h1.mega', '.intro-text'],
+        opacity: 1,
+        delay: anime.stagger(100)
+    }).add({
+        targets: '.navbar-brand span',
+        opacity: 1,
+        delay: 100
+      }).add({
+        targets: '.navbar-menu .navbar-item',
+        opacity: 1,
+        delay: [220, anime.stagger(250)] 
+      })
+}, 5000);
+};
+
+preload();
